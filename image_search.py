5e4c4855550de4aa289f3ae1e776a9f920eee0ce
shivaniyadav2006f-pixel <shivaniyadav2006f@gmail.com>
2026-07-22 20:23:42 +0530

    Add files via upload

--- image_search.py
+++ image_search.py
@@ -1,8 +1,5 @@
-import requests
-from config import UNSPLASH_ACCESS_KEY
-
-
 def search_image(query):
+    import requests
 
     url = "https://api.unsplash.com/search/photos"
 
@@ -15,18 +12,21 @@ def search_image(query):
         "per_page": 1
     }
 
-    response = requests.get(
-        url,
-        headers=headers,
-        params=params
-    )
+    response = requests.get(url, headers=headers, params=params)
 
+    print("=" * 50)
     print("Status Code:", response.status_code)
     print("Response:", response.text)
+    print("=" * 50)
+
+    if response.status_code != 200:
+        return None
 
     data = response.json()
 
-    if data.get("results"):
+    if len(data["results"]) > 0:
+        print("IMAGE URL:", data["results"][0]["urls"]["regular"])
         return data["results"][0]["urls"]["regular"]
 
+    print("No image found")
     return None
